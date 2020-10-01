80a2f1d99a15c6f78951da092ece85285e0a3b91
TAOSAND <taofeekdboy@yahoo.com>
2020-10-01 11:32:43 +0400

    Updates

---
+++ precache-manifest.17d2116c0c049fbbde7e2e92bcf1236c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894db80f2ee5f5b4882a357603e04f1e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db05d0b555bff5cd1896",
+    "url": "/portfolio/static/css/main.57d6ac81.chunk.css"
+  },
+  {
+    "revision": "131829a4ac1dda84b78e",
+    "url": "/portfolio/static/js/2.0f24a4d9.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/portfolio/static/js/2.0f24a4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db05d0b555bff5cd1896",
+    "url": "/portfolio/static/js/main.9bd3ce22.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
